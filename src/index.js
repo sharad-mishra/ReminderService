f935326b736e89c8003a514b440eac442fa1784d
Sharad Mishra <sharadrx9@gmail.com>
2025-06-04 12:41:41 +0000

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { PORT} = require('./config/serverConfig');
-const { sendBasicEmail } = require('./services/email-service')
+const { PORT } = require('./config/serverConfig');
+const { sendBasicEmail } = require('./services/email-service');
+//const { sendBasicEmail } = require('./services/email-service');
+const cron = require('node-cron');
+
 
 const setupAndStartServer = () => {
     const app = express();
@@ -10,12 +13,15 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, ()=>{
         console.log(`Server is running on port ${PORT}`);
-        sendBasicEmail(
-            'support@airline.com',
-            'airline.booking.backend.email@gmail.com',
-            'Test Email',
-            'This is a test email sent from the Reminder Service.'
-        )
+        // sendBasicEmail(
+        //     'support@airline.com',
+        //     'airline.booking.backend.email@gmail.com',
+        //     'Test Email',
+        //     'This is a test email sent from the Reminder Service.'
+        // );
+        cron.schedule('*/2 * * * *', ()=>{
+            console.log('running a task every two minutes ');
+        });
     });
 }
 
